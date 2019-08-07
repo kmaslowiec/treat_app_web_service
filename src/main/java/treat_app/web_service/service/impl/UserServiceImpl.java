d8dad60ae55f1e7cb9cfdff8f2d449a4c7f5deb7
Konrad Maslowiec <kmaslowiec@gmail.com>
2019-08-07 15:05:23 +0200

    create_newUser_validDto_returnsDtoWithSavedValues test impl

--- src/main/java/treat_app/web_service/service/impl/UserServiceImpl.java
+++ src/main/java/treat_app/web_service/service/impl/UserServiceImpl.java
@@ -2,11 +2,15 @@ package treat_app.web_service.service.impl;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import treat_app.web_service.entity.Treat;
 import treat_app.web_service.entity.User;
 import treat_app.web_service.repository.UserRepo;
 import treat_app.web_service.service.UserService;
+import treat_app.web_service.service.dto.TreatDto;
 import treat_app.web_service.service.dto.UserDto;
 import treat_app.web_service.service.mapper.UserMapper;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -17,9 +21,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto create(UserDto userDto) {
+        List<Treat> treats = userMapper.toTreatEntities(userDto.getTreatDtos());
         User user = userMapper.toEntity(userDto);
+        user.setTreats(treats);
+
         User savedUser = userRepo.save(user);
-        return userMapper.toDto(savedUser);
+
+        UserDto returnedDto = userMapper.toDto(savedUser);
+        List<TreatDto> treatDtos = userMapper.toTreatDtos(savedUser.getTreats());
+        returnedDto.setTreatDtos(treatDtos);
+
+        return returnedDto;
     }
 
     @Override
---
+++ src/test/java/treat_app/web_service/service/impl/UserServiceImplTest.java
@@ -0,0 +1,71 @@
+package treat_app.web_service.service.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit4.SpringRunner;
+import treat_app.web_service.ObjectFactory;
+import treat_app.web_service.entity.Treat;
+import treat_app.web_service.entity.User;
+import treat_app.web_service.repository.UserRepo;
+import treat_app.web_service.service.dto.TreatDto;
+import treat_app.web_service.service.dto.UserDto;
+import treat_app.web_service.service.mapper.UserMapper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class UserServiceImplTest {
+
+    @Mock
+    private UserRepo userRepo;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserServiceImpl service;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void create_newUser_validDto_returnsDtoWithSavedValues() {
+        //given
+        List<TreatDto> treatsInEnteredDto = new ArrayList<>(Arrays.asList(ObjectFactory.TreatDto_userId(1L), ObjectFactory.TreatDto_id_name_userId(2L, "second", 1L)));
+        UserDto enterDto = ObjectFactory.UserDto();
+        enterDto.setTreatDtos(treatsInEnteredDto);
+        User user = ObjectFactory.User();
+        List<Treat> treatsDtosFromUser = new ArrayList<>(Arrays.asList(ObjectFactory.Treat_user(user), ObjectFactory.Treat_id_name_user(2L, "second", user)));
+        user.setTreats(treatsDtosFromUser);
+
+        User savedUser = ObjectFactory.User();
+        UserDto returnedDto = ObjectFactory.UserDto();
+
+        //when
+        when(userMapper.toTreatEntities(enterDto.getTreatDtos())).thenReturn(treatsDtosFromUser);
+        when(userMapper.toEntity(enterDto)).thenReturn(user);
+        when(userRepo.save(user)).thenReturn(savedUser);
+        when(userMapper.toDto(savedUser)).thenReturn(returnedDto);
+        when(userMapper.toTreatDtos(savedUser.getTreats())).thenReturn(treatsInEnteredDto);
+
+        UserDto testedDto = service.create(enterDto);
+        //then
+        assertThat(testedDto).isNotNull();
+        assertThat(testedDto.getTreatDtos()).isEqualTo(enterDto.getTreatDtos());
+    }
+
+    @Test
+    public void getByid() {
+    }
+}
