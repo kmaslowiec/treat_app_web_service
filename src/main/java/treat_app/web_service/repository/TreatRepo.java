6b1b117cdbd9c4b359f2de8a2cd76391c43d6e99
Konrad Maslowiec <kmaslowiec@gmail.com>
2019-08-20 14:36:38 +0200

    TreatService currently not required. Treats will be saved in UserServiceImpl

---
+++ src/main/java/treat_app/web_service/repository/TreatRepo.java
@@ -0,0 +1,17 @@
+package treat_app.web_service.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import treat_app.web_service.entity.Treat;
+import treat_app.web_service.entity.User;
+
+import java.util.List;
+
+@Repository
+public interface TreatRepo extends CrudRepository<Treat, Long> {
+
+    @Override
+    <S extends Treat> Iterable<S> saveAll(Iterable<S> entities);
+
+    List<Treat> findAllByUser(List<User> users);
+}
